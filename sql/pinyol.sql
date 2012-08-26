50b4f647302663fea9bc07c515895b3a7aa660e8
julian-git <julian-git@gmx.de>
2012-08-27 01:37:58 +0200

    documented the definition of the database

--- sql/pinyol.sql
+++ sql/pinyol.sql
@@ -1,3 +1,6 @@
+/**
+ *  General information about the colla
+ */
 create table colla (
   id   	     int   not null auto_increment,
   name 	     varchar(255) not null,
@@ -10,12 +13,19 @@ create table colla (
   primary key (id)
 ) engine=InnoDB default character set utf8;
 
+/**
+ *  The different roles in a castell: crosses, mans, vents, ...
+ */
 create table role (
   id   	     int	not null auto_increment,
   name 	     varchar(100) not null,
   primary key (id)
 ) engine=InnoDB default character set utf8;
 
+/**
+ *   Some information about the members of a colla, 
+ *   especially their physical characteristics
+ */
 create table member (
   id   	     int	not null auto_increment,
   name 	     varchar(100) not null,
@@ -27,6 +37,9 @@ create table member (
   primary key (id)
 ) engine=InnoDB default character set utf8;
 
+/** 
+ *   The collas that a certain member belongs to
+ */
 create table member_colla (
   member_id  int  not null,
   colla_id   int  not null,
@@ -34,6 +47,9 @@ create table member_colla (
   foreign key (colla_id) references colla(id)
 ) engine=InnoDB default character set utf8;
 
+/**
+ *  The roles that a certain member can fulfill in a castell
+ */
 create table member_role (
   member_id  int  not null,
   role_id   int  not null,
@@ -41,6 +57,9 @@ create table member_role (
   foreign key (role_id) references role(id)
 ) engine=InnoDB default character set utf8;
 
+/** 
+ *  The different types of castell that each colla does: p4, 2de8f, ...
+ */
 create table castell_type (
   id   	     int	not null auto_increment,     
   colla_id   int        not null,
@@ -50,6 +69,9 @@ create table castell_type (
   foreign key (colla_id) references colla(id)
 ) engine=InnoDB default character set utf8;
 
+/** 
+ *  The data for displaying the positions in each type of castell
+ */ 
 create table castell_position (
   id   	     int	not null auto_increment,     
   castell_type_id int 	not null,
@@ -67,18 +89,49 @@ create table castell_position (
   foreign key (role_id) references role (id)
 ) engine=InnoDB default character set utf8;
 
+/**
+ *  The data for establishing relations between two positions in a given castell
+ */
+create table castell_relation (
+  id   	       int  not null auto_increment,
+  castell_type_id int 	not null,
+  from_position int not null,
+  to_position   int default null,
+  weight 	float(10,4) default 0,
+  primary key (id),
+  foreign key (castell_type_id) references castell_type (id),
+  foreign key (from_position) references castell_position (id),
+  foreign key (to_position) references castell_position (id)
+) engine=InnoDB default character set utf8;
 
+/**
+ *  Which castells has a colla executed, and with which result?
+ */ 
 create table executed_castell (
   id   	       int  not null auto_increment,
   colla_id     int  not null,
   castell_type_id int not null,
-  ts 	       timestamp default current_timestamp,
+  execution_date  timestamp default current_timestamp,
+  result       varchar(20) default null,
+  comment      varchar(255) default null,
+  primary key (id),
+  foreign key (colla_id) references colla(id),
+  foreign key (castell_type_id) references castell_type (id)
+) engine=InnoDB default character set utf8;
+
+/** 
+ *  What has each member done in each executed castell, 
+ *  and what was his strength that day?
+ */
+create table executed_castell_position (
+  id   	       int  not null auto_increment,
+  executed_castell_id int not null,
   member_id    int  not null,
   role_id      int  not null,
   position_id  int  not null,
+  strength     int default 5,
   primary key (id),
-  foreign key (colla_id) references colla(id),
-  foreign key (castell_type_id) references castell_type (id),
+  foreign key (executed_castell_id) references executed_castell(id),
   foreign key (member_id) references member(id),
   foreign key (role_id) references role (id),
   foreign key (position_id) references castell_position (id)
