b63182359380593873b12886e6b3cc07f5c3b761
julian-git <julian-git@gmx.de>
2012-08-31 17:30:53 +0200

    small

--- run_unit_tests.py
+++ run_unit_tests.py
@@ -5,17 +5,13 @@ import unittest
 import build_ip
 import solve_ip
 
-make_lp_file = build_ip.make_lp_file
-ip_ineqs = build_ip.ip_ineqs
-find_pinya = build_ip.find_pinya
-
 class IPTest(unittest.TestCase):
 
     def test_build_ip(self):
         f = open('tests/test.lp', 'r')
         participation = dict([(9, 0), (17, 5)])
         castellers_in_position = dict()
-        self.assertEqual(make_lp_file(ip_ineqs(castellers_in_position, participation)), f.read())
+        self.assertEqual(build_ip.make_lp_file(build_ip.ip_ineqs(castellers_in_position, participation)), f.read())
 
     def test_solve_ip(self):
         f = open('tests/test.solutions', 'r')
@@ -25,7 +21,7 @@ class IPTest(unittest.TestCase):
     def test_find_pinya(self):
         f = open('tests/test.find_pinya', 'r')
         participation = dict([(9, 0), (17, 5)])
-        self.assertEqual(str(find_pinya(participation)) + "\n", f.read())
+        self.assertEqual(str(build_ip.find_pinya(participation)) + "\n", f.read())
         
 
 unittest.main() # Calling from the command line invokes all tests
