b48e3e677edb1928e3496b41d80e1fc0c7d38b11
julian-git <julian-git@gmx.de>
2012-09-03 18:13:48 +0200

    deleted unnecessary file

--- run_unit_tests.py
+++ run_unit_tests.py
@@ -1,6 +1,5 @@
 import unittest
 import build_ip
-import solve_ip
 
 class IPTest(unittest.TestCase):
 
@@ -13,10 +12,6 @@ class IPTest(unittest.TestCase):
         ineqs = []
         build_ip.ip_ineqs(castellers_in_position, position_data, obj_val, ineqs, participation)
         self.assertEqual(build_ip.make_lp_file(obj_val, ineqs), f.read())
-
-    def test_solve_ip(self):
-        f = open('tests/test.solutions', 'r')
-        self.assertEqual(str(solve_ip.get_solutions("tests/test.lp")), f.read())
 
     def test_find_pinya(self):
         f = open('tests/test.find_pinya', 'r')
