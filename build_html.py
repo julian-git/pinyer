1330cc79b5d1389d24c16b1b99bf57bfab526e49
julian-git <julian-git@gmx.de>
2012-09-06 19:49:30 +0200

    removed liteAccordion again

--- build_html.py
+++ build_html.py
@@ -2,18 +2,18 @@ from build_ip import find_pinya
 import html_common 
 from db_interaction import get_db, get_positions, get_castell
 
-def fill_in(pd, svg, svgclass, min_x, max_x, min_y, max_y, name='', dyn_props=''):
+def fill_in(pd, svg, svgclass, min_x, max_x, min_y, max_y, name=''):
     if pd['svg_elem'] == 'rect':
         svg = svg + \
             html_common.svg_rect.substitute(_svg_id=pd['svg_id'], _class=svgclass, \
-                                                _dyn_props=dyn_props, _name=name, \
+                                                _name=name, \
                                                 _x=pd['x'], _y=pd['y'], _w=pd['w'], _h=pd['h'])
         min_x = min(min_x, pd['x']); max_x = max(max_x, pd['x']+pd['w'])
         min_y = min(min_y, pd['y']); max_y = max(max_y, pd['y']+pd['h'])
     elif pd['svg_elem'] == 'circle':
         svg = svg + \
             html_common.svg_circle.substitute(_svg_id=pd['svg_id'], _class=svgclass, \
-                                                  _dyn_props=dyn_props, _name=name, \
+                                                  _name=name, \
                                                   _x=pd['x'], _y=pd['y'], _rx=pd['rx'])
         min_x = min(min_x, pd['x']-pd['rx']); max_x = max(max_x, pd['x']+pd['rx'])
         min_y = min(min_y, pd['y']-pd['rx']); max_y = max(max_y, pd['y']+pd['rx'])
@@ -28,13 +28,12 @@ def center_image(min_x, max_x, min_y, max_y):
 def editable_castell_plan(castell_type_id):
     db = get_db()
     position_data = get_positions(db, castell_type_id)
-    dyn_props = ' class="ui-draggable" '
     svg = ''
     svgclass = 'design'
     [min_x, max_x, min_y, max_y] = [1000000, -1000000, 1000000, -1000000]
     for pos, pd in position_data.iteritems():
         [svg, min_x, max_x, min_y, max_y] = \
-            fill_in(pd, svg, svgclass, min_x, max_x, min_y, max_y, pd['role_name'], dyn_props)
+            fill_in(pd, svg, svgclass, min_x, max_x, min_y, max_y, pd['role_name'])
     [min_x, max_x, min_y, max_y] = center_image(min_x, max_x, min_y, max_y)
     viewbox = \
         'viewBox="' + str(min_x) + ' ' + str(min_y) + ' ' + \
