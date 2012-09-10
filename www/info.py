84bebd43f22048e98af71287498c8333765b36ac
julian-git <julian-git@gmx.de>
2012-09-10 18:53:55 +0200

    sanitizing input

--- www/info.py
+++ www/info.py
@@ -19,17 +19,26 @@ what = form["what"].value
 
 db = db_interaction.get_db()
 
+def sanitize(form, arg, whitelist):
+    if arg not in form.keys():
+        raise Exception("Need argument for ", arg)
+    val = form[arg].value
+    if val in whitelist:
+        return val
+    else:
+        raise Exception("unexpected argument: ", val)
+        
+
+
 print 'Content-Type: application/json, charset="utf-8"'     # HTML is following
 print                               # blank line, end of headers
 
 if what=='get_colla':
     colla_id = form["colla_id"].value
-    _char = form["char"].value
-    if _char in ['total_height', 'shoulder_height', 'axle_height', 'hip_height', 'stretched_height', 'shoulder_height', 'shoulder_width']:
-        char=_char
-    else:
-        raise Exception("unexpected argument: ", _char)
-        
+    char = sanitize(form, "char", 
+                    ['total_height', 'shoulder_height', \
+                         'axle_height', 'hip_height', 'stretched_height', \
+                         'shoulder_height', 'shoulder_width'])
     colla = db_interaction.get_nicknames_and_char(db, colla_id, char)
     print json.dumps(colla, separators=(',', ':'), ensure_ascii=False)
 
