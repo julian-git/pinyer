c68d2d8887c9dd8a66a5ed4947d0fc1ba9f2975c
julian-git <julian-git@gmx.de>
2012-09-11 00:34:42 +0200

    started with editing pinya

--- www/info.py
+++ www/info.py
@@ -42,5 +42,10 @@ if what=='get_colla':
     colla = db_interaction.get_nicknames_and_char(db, colla_id, char)
     print json.dumps(colla, separators=(',', ':'), ensure_ascii=False)
 
+if what=='get_pinya':
+    pinya_id = form["pinya_id"].value
+    pinya = db_interaction.get_positions(db, pinya_id)
+    print json.dumps(pinya, separators=(',', ':'), ensure_ascii=False)
+
 else:
     print "Unexpected argument: ", what
